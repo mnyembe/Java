2918c430608d5a379fde652e58a1d8bd02c03d91
Pocket Studio <admin@pocketstudio.co.za>
2021-12-21 13:57:39 +0200

    BarkingDog commit checked

---
+++ EqualSumChecker.java
@@ -0,0 +1,6 @@
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int x, int y, int z){
+        boolean checkSum = ((x + y) == z) ? true : false;
+        return checkSum;
+    }
+}
