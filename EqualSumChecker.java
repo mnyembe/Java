7cb79a5d66ea6a9cc91f6596cbd8e0ac4d10f977
Pocket Studio <admin@pocketstudio.co.za>
2021-12-22 09:29:10 +0200

    EqualSumChecker commit checked

--- EqualSumChecker.java
+++ EqualSumChecker.java
@@ -1,6 +1,6 @@
 public class EqualSumChecker {
     public static boolean hasEqualSum(int x, int y, int z){
-        boolean checkSum = ((x + y) == z) ? true : false;
+        boolean checkSum = (x + y) == z ? true : false;
         return checkSum;
     }
 }
